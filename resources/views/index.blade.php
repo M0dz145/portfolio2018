81bc898d7a1f29e9d3a86b4394b52acd02327e1c
CHEVALIER Xavier <xchevalier@valeur-et-capital.com>
2017-11-16 22:49:56 +0100

    Initialize laravel project

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>| Xavier Chevalier Portfolio |</title>
+</head>
+<body id="app">
+    <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deserunt dolor dolores doloribus enim error fugiat in laudantium maiores nihil quam quasi, quidem saepe, soluta velit veniam? Iure, necessitatibus nemo! Nulla?
+    </p>
+
+    <script src="js/app.js"></script>
+</body>
+</html>
