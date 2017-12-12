d3fc0cd785bcf09a3f5b1b7f0ac588b579575efc
CHEVALIER Xavier <xchevalier@valeur-et-capital.com>
2017-12-12 23:35:28 +0100

    Fix TiltFx & change to class

--- resources/assets/js/components/TiltFx.js
+++ resources/assets/js/components/TiltFx.js
@@ -1,55 +1,160 @@
 import Anime from 'animejs';
 
 export default class TiltFx {
-    /**
-     * TiltFx obj.
-     */
-    constructor(el, options) {
-        this.DOM = {};
-        this.DOM.el = el;
-        this.options = TiltFx.extend({}, this.options);
-        TiltFx.extend(this.options, options);
-
+    constructor(element, options) {
+        this.element = element;
         this.options = {
-            movement: {
-                imgWrapper: {
-                    translation: {x: 0, y: 0, z: 0},
-                    rotation: {x: -5, y: 5, z: 0},
-                    reverseAnimation: {
-                        duration: 1200,
-                        easing: 'easeOutElastic',
-                        elasticity: 600
-                    }
-                },
-                lines: {
-                    translation: {x: 10, y: 10, z: [0, 10]},
-                    reverseAnimation: {
-                        duration: 1000,
-                        easing: 'easeOutExpo',
-                        elasticity: 600
-                    }
-                },
-                caption: {
-                    translation: {x: 20, y: 20, z: 0},
-                    rotation: {x: 0, y: 0, z: 0},
-                    reverseAnimation: {
-                        duration: 1500,
-                        easing: 'easeOutElastic',
-                        elasticity: 600
-                    }
-                },
-                shine: {
-                    translation: {x: 50, y: 50, z: 0},
-                    reverseAnimation: {
-                        duration: 1200,
-                        easing: 'easeOutElastic',
-                        elasticity: 600
-                    }
-                }
-            }
+            movement: []
         };
 
-        this._init();
+        TiltFx.extend(this.options, options);
+
+        // this.options = {
+        //     movement: {
+        //         imgWrapper: {
+        //             translation: {x: 0, y: 0, z: 0},
+        //             rotation: {x: -5, y: 5, z: 0},
+        //             reverseAnimation: {
+        //                 duration: 1200,
+        //                 easing: 'easeOutElastic',
+        //                 elasticity: 600
+        //             }
+        //         },
+        //         lines: {
+        //             translation: {x: 10, y: 10, z: [0, 10]},
+        //             reverseAnimation: {
+        //                 duration: 1000,
+        //                 easing: 'easeOutExpo',
+        //                 elasticity: 600
+        //             }
+        //         },
+        //         caption: {
+        //             translation: {x: 20, y: 20, z: 0},
+        //             rotation: {x: 0, y: 0, z: 0},
+        //             reverseAnimation: {
+        //                 duration: 1500,
+        //                 easing: 'easeOutElastic',
+        //                 elasticity: 600
+        //             }
+        //         },
+        //         shine: {
+        //             translation: {x: 50, y: 50, z: 0},
+        //             reverseAnimation: {
+        //                 duration: 1200,
+        //                 easing: 'easeOutElastic',
+        //                 elasticity: 600
+        //             }
+        //         }
+        //     }
+        // };
+        this.element.addEventListener('mousemove', (ev) => requestAnimationFrame(() => this.mouseMove(ev)));
+
+        if(this.options.mouseLeave) {
+            this.element.addEventListener('mouseleave', () => requestAnimationFrame(() => this.mouseLeave.call(this)));
+            this.element.addEventListener('mouseenter', () => this.mouseEnter.call(this));
+        }
+    }
+
+    addElement(element, options) {
+        this.options.movement.push({
+            element: this.element.querySelector(element),
+            ...options
+        });
+
+        return this;
+    }
+
+    mouseEnter() {
+        this.options.movement.forEach(movement => Anime.remove(movement.element));
+    }
+
+    mouseLeave() {
+        this.options.movement.forEach(movement => Anime({
+            targets: movement.element,
+            duration: typeof movement.reverseAnimation !== 'undefined' ? movement.reverseAnimation.duration || 0 : 1,
+            easing: typeof movement.reverseAnimation !== 'undefined' ? movement.reverseAnimation.easing || 'linear' : 'linear',
+            elasticity: typeof movement.reverseAnimation !== 'undefined' ? movement.reverseAnimation.elasticity || null : null,
+            scaleX: 1,
+            scaleY: 1,
+            scaleZ: 1,
+            translateX: 0,
+            translateY: 0,
+            translateZ: 0,
+            rotateX: 0,
+            rotateY: 0,
+            rotateZ: 0
+        }));
+    }
+
+    mouseMove(ev) {
+        // Mouse position relative to the document.
+        const mousepos    = TiltFx.getMousePos(ev),
+              // Document scrolls.
+              docScrolls  = {
+                  left: document.body.scrollLeft + document.documentElement.scrollLeft,
+                  top: document.body.scrollTop + document.documentElement.scrollTop
+              },
+              bounds      = this.element.getBoundingClientRect(),
+              // Mouse position relative to the main element (this.element).
+              relmousepos = {
+                  x: mousepos.x - bounds.left - docScrolls.left,
+                  y: mousepos.y - bounds.top - docScrolls.top
+              };
+
+        this.options.movement.forEach(movement => {
+            const translation = movement.translation || TiltFx.vector3(0, 0, 0),
+                  rotation    = movement.rotation || TiltFx.vector3(0, 0, 0);
+
+            TiltFx.setRange(translation);
+            TiltFx.setRange(rotation);
+
+            const transforms = {
+                translation: {
+                    x: (translation.x[1] - translation.x[0]) / bounds.width * relmousepos.x + translation.x[0],
+                    y: (translation.y[1] - translation.y[0]) / bounds.height * relmousepos.y + translation.y[0],
+                    z: (translation.z[1] - translation.z[0]) / bounds.height * relmousepos.y + translation.z[0],
+                },
+                rotation: {
+                    x: (rotation.x[1] - rotation.x[0]) / bounds.height * relmousepos.y + rotation.x[0],
+                    y: (rotation.y[1] - rotation.y[0]) / bounds.width * relmousepos.x + rotation.y[0],
+                    z: (rotation.z[1] - rotation.z[0]) / bounds.width * relmousepos.x + rotation.z[0]
+                }
+            };
+
+            // let animations = [];
+            // if(movement.anime) {
+            //     movement.anime.animations.forEach(animation => {
+            //         animations[animation.property] = typeof animation.currentValue !== 'number' ? Number(animation.currentValue.replace(/px/, '')) : animation.currentValue;
+            //     });
+            //
+            //     Anime.remove(movement.element);
+            //
+            //     movement.element.style.WebkitTransform = movement.element.style.transform = `
+            //         translateX(${animations['translateX']}px)
+            //         translateY(${animations['translateY']}px)
+            //         translateZ(${animations['translateZ']}px)
+            //         rotateX(${animations['rotationX']}deg)
+            //         rotateY(${animations['rotationY']}deg)
+            //         rotateZ(${animations['rotationZ']}deg)
+            //     `;
+            // }
+
+            movement.anime = Anime({
+                targets: movement.element,
+                scaleX: 1,
+                scaleY: 1,
+                scaleZ: 1,
+                translateX: transforms.translation.x,
+                translateY: transforms.translation.y,
+                translateZ: transforms.translation.z,
+                rotateX: transforms.rotation.x,
+                rotateY: transforms.rotation.y,
+                rotateZ: transforms.rotation.z,
+                duration: movement.duration || 0,
+                elasticity: movement.elasticity || 0,
+                easing: movement.easing || 'easeOutCirc'
+            });
+        });
     }
 
     // Helper vars and functions.
@@ -90,104 +195,11 @@ export default class TiltFx {
         return {x: posx, y: posy}
     }
 
-    _init() {
-        this.DOM.animatable = {};
-        this.DOM.animatable.imgWrapper = this.DOM.el.querySelector('.panelContainer__img');
-        this.DOM.animatable.lines = this.DOM.el.querySelector('.panel__content');
-        // this.DOM.animatable.caption = this.DOM.el.querySelector('.tilter__caption');
-        // this.DOM.animatable.overlay = this.DOM.el.querySelector('.tilter__deco--overlay');
-        // this.DOM.animatable.shine = this.DOM.el.querySelector('.tilter__deco--shine > div');
-        this._initEvents();
-    }
-
-    /**
-     * Init/Bind events.
-     */
-    _initEvents() {
-        const self = this;
-
-        this.DOM.el.addEventListener('mousemove', (ev) => requestAnimationFrame(() => this._layout(ev)));
-
-        this.DOM.el.addEventListener('mouseleave', (ev) => requestAnimationFrame(() => {
-            for(const key in self.DOM.animatable) {
-                if(typeof self.options.movement[key] === 'undefined') {
-                    continue;
-                }
-
-                Anime({
-                    targets: self.DOM.animatable[key],
-                    duration: typeof self.options.movement[key].reverseAnimation !== 'undefined' ? self.options.movement[key].reverseAnimation.duration || 0 : 1,
-                    easing: typeof self.options.movement[key].reverseAnimation !== 'undefined' ? self.options.movement[key].reverseAnimation.easing || 'linear' : 'linear',
-                    elasticity: typeof self.options.movement[key].reverseAnimation !== 'undefined' ? self.options.movement[key].reverseAnimation.elasticity || null : null,
-                    scaleX: 1,
-                    scaleY: 1,
-                    scaleZ: 1,
-                    translateX: 0,
-                    translateY: 0,
-                    translateZ: 0,
-                    rotateX: 0,
-                    rotateY: 0,
-                    rotateZ: 0
-                });
-            }
-        }));
-
-        this.DOM.el.addEventListener('mouseenter', (ev) => {
-            for(const key in self.DOM.animatable) {
-                Anime.remove(self.DOM.animatable[key]);
-            }
-        });
-    }
-
-    static getResetPosition() {
+    static vector3(x, y, z) {
         return {
-            x: 0,
-            y: 0,
-            z: 0
+            x,
+            y,
+            z
         };
-    }
-
-    _layout(ev) {
-        // Mouse position relative to the document.
-        const mousepos    = TiltFx.getMousePos(ev),
-              // Document scrolls.
-              docScrolls  = {
-                  left: document.body.scrollLeft + document.documentElement.scrollLeft,
-                  top: document.body.scrollTop + document.documentElement.scrollTop
-              },
-              bounds      = this.DOM.el.getBoundingClientRect(),
-              // Mouse position relative to the main element (this.DOM.el).
-              relmousepos = {
-                  x: mousepos.x - bounds.left - docScrolls.left,
-                  y: mousepos.y - bounds.top - docScrolls.top
-              };
-
-        // Movement settings for the animatable elements.
-        for(const key in this.DOM.animatable) {
-            if(typeof this.DOM.animatable[key] === 'undefined' || typeof this.options.movement[key] === 'undefined') {
-                continue;
-            }
-
-            const t = typeof this.options.movement[key] !== 'undefined' ? this.options.movement[key].translation || TiltFx.getResetPosition() : TiltFx.getResetPosition(),
-                  r = typeof this.options.movement[key] !== 'undefined' ? this.options.movement[key].rotation || TiltFx.getResetPosition() : TiltFx.getResetPosition();
-
-            TiltFx.setRange(t);
-            TiltFx.setRange(r);
-
-            const transforms = {
-                translation: {
-                    x: (t.x[1] - t.x[0]) / bounds.width * relmousepos.x + t.x[0],
-                    y: (t.y[1] - t.y[0]) / bounds.height * relmousepos.y + t.y[0],
-                    z: (t.z[1] - t.z[0]) / bounds.height * relmousepos.y + t.z[0],
-                },
-                rotation: {
-                    x: (r.x[1] - r.x[0]) / bounds.height * relmousepos.y + r.x[0],
-                    y: (r.y[1] - r.y[0]) / bounds.width * relmousepos.x + r.y[0],
-                    z: (r.z[1] - r.z[0]) / bounds.width * relmousepos.x + r.z[0]
-                }
-            };
-
-            this.DOM.animatable[key].style.WebkitTransform = this.DOM.animatable[key].style.transform = 'translateX(' + transforms.translation.x + 'px) translateY(' + transforms.translation.y + 'px) translateZ(' + transforms.translation.z + 'px) rotateX(' + transforms.rotation.x + 'deg) rotateY(' + transforms.rotation.y + 'deg) rotateZ(' + transforms.rotation.z + 'deg)';
-        }
     }
 }
--- resources/assets/js/components/VSlide.vue
+++ resources/assets/js/components/VSlide.vue
@@ -5,14 +5,39 @@
     export default {
         data() {
             return {
-                ElkyosImage: require('@img/works/nicolasChevalier.jpg?sizes[]=200,sizes[]=600,sizes[]=800')
+                ElkyosImage: require('@img/works/nicolasChevalier.jpg?placeholder=true&sizes[]=200,sizes[]=600,sizes[]=800')
             };
         },
         mounted() {
-            new TiltFx(document.getElementsByClassName('slide')[0]);
-        },
-        methods: {
+            const TiltFxInstance = new TiltFx(document.getElementsByClassName('slide')[0], {
+                mouseLeave: false
+            });
 
+            TiltFxInstance
+                .addElement('.panel__content', {
+                    translation: TiltFx.vector3(10, 10, 0),
+                    rotation: TiltFx.vector3(5, 5, 0),
+                    duration: 800,
+                    elasticity: 100,
+                    easing: 'easeOutCirc',
+                    reverseAnimation: {
+                        duration: 1200,
+                        easing: 'easeOutCirc',
+                        elasticity: 600
+                    }
+                })
+                .addElement('.panelContainer__img', {
+                    translation: TiltFx.vector3(5, 5, 0),
+                    rotation: TiltFx.vector3(5, 5, 0),
+                    duration: 800,
+                    elasticity: 400,
+                    easing: 'easeOutCirc',
+                    reverseAnimation: {
+                        duration: 1000,
+                        easing: 'easeOutCirc',
+                        elasticity: 800
+                    }
+                });
         },
         components: {
             VImage
@@ -28,15 +53,16 @@
                     <div class="panel__text">
                         <h2 class="panel__title">Elkyos</h2>
                         <p class="panel__info">
-                            MMORPG médiéval fantastique
+                            MMORPG médiéval fantastique, 2014
                         </p>
                     </div>
                     <div class="panel__line"></div>
+                    <router-link :to="{name: 'works'}" class="panel__link">En savoir plus</router-link>
                 </div>
             </div>
             <div class="panelContainer__img">
                 <VImage class="panel__img" :src="ElkyosImage.src" :srcSet="ElkyosImage.srcSet"
-                        :styles="{width: '100%', height: '100%'}"
+                        :styles="{backgroundImage: `url(${ElkyosImage.placeholder})`, width: '100%', height: '100%'}"
                         alt="Description"></VImage>
             </div>
         </div>
@@ -44,13 +70,13 @@
 </template>
 
 <style lang="scss" scoped>
+    @import "./../../sass/variables";
     @import "./../../sass/mixins/mixins";
 
     .slide {
         position: absolute;
         width: 100%;
         height: 100%;
-        perspective: 500px;
         @include no-user-select();
 
         .panel {
@@ -58,18 +84,30 @@
             position: relative;
             top: 50%;
             left: 50%;
-            width: 100%;
-            max-width: 1200px;
+            width: 50%;
+            max-width: 800px;
             height: 466px;
             transform: translate3d(-50%, -50%, 0);
+
+            &:hover {
+                .panelContainer__content {
+                    .panel__content {
+                        .panel__link {
+                            opacity: 1;
+                            transform: translate3d(0, 0, 0);
+                            transition: transform .4s $easeOutCubic, opacity .4s $easeOutCubic;
+                        }
+                    }
+                }
+            }
 
             .panelContainer__content {
                 .panel__content {
                     z-index: z(slidePanelContent);
                     position: absolute;
                     top: 26%;
-                    left: 0;
-                    width: 100%;
+                    left: -8%;
+                    width: 115%;
 
                     .panel__text {
                         display: inline-block;
@@ -78,27 +116,47 @@
 
                         .panel__title {
                             font-size: 3em;
+                            font-family: font-family(Aventura);
+                            text-transform: uppercase;
+                            text-shadow: 0 2px 8px rgba(0, 0, 0, .4);
                         }
 
                         .panel__info {
                             position: relative;
                             margin: 16px 0 0;
+                            text-shadow: 0 2px 8px rgba(0, 0, 0, .4);
 
                             &::before {
                                 content: '';
+                                display: inline-block;
                                 margin: 9px 14px 0 0;
                                 height: 1px;
                                 width: 30px;
+                                vertical-align: top;
                                 background-color: white;
                             }
                         }
                     }
 
                     .panel__line {
-                        margin: 24px 0 0 40%;
-                        width: 64%;
+                        margin: 24px 0 0 25%;
+                        width: 75%;
                         height: 3px;
                         background-color: white;
+                        box-shadow: 0 2px 8px rgba(0, 0, 0, .4);
+                    }
+
+                    .panel__link {
+                        position: absolute;
+                        margin-top: 10px;
+                        right: 10%;
+                        color: white;
+                        opacity: 0;
+                        font-size: 1.2em;
+                        text-decoration: none;
+                        text-shadow: 0 2px 8px rgba(0, 0, 0, .4);
+                        transform: translate3d(0, -25px, 0);
+                        transition: transform .4s $easeOutCubic, opacity .2s $easeOutCubic;
                     }
                 }
             }
@@ -113,6 +171,7 @@
                     display: block;
                     width: 100%;
                     min-height: 100%;
+                    transform: translate3d(0, -25%, 0);
                     @include no-pointer-events();
                 }
             }
