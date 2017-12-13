e060061efe27b9f3ee867ad43f493427bc1181f9
CHEVALIER Xavier <mcmodz@hotmail.fr>
2017-12-13 12:06:20 +0100

    Add appear animation slide && chevron left & right

--- resources/assets/js/components/VSlide.vue
+++ resources/assets/js/components/VSlide.vue
@@ -8,37 +8,6 @@
                 ElkyosImage: require('@img/works/nicolasChevalier.jpg?placeholder=true&sizes[]=200,sizes[]=600,sizes[]=800')
             };
         },
-        mounted() {
-            const TiltFxInstance = new TiltFx(document.getElementsByClassName('slide')[0], {
-                mouseLeave: false
-            });
-
-            TiltFxInstance
-                .addElement('.panel__content', {
-                    translation: TiltFx.vector3(10, 10, 0),
-                    rotation: TiltFx.vector3(5, 5, 0),
-                    duration: 800,
-                    elasticity: 100,
-                    easing: 'easeOutCirc',
-                    reverseAnimation: {
-                        duration: 1200,
-                        easing: 'easeOutCirc',
-                        elasticity: 600
-                    }
-                })
-                .addElement('.panelContainer__img', {
-                    translation: TiltFx.vector3(5, 5, 0),
-                    rotation: TiltFx.vector3(5, 5, 0),
-                    duration: 800,
-                    elasticity: 400,
-                    easing: 'easeOutCirc',
-                    reverseAnimation: {
-                        duration: 1000,
-                        easing: 'easeOutCirc',
-                        elasticity: 800
-                    }
-                });
-        },
         components: {
             VImage
         }
@@ -46,25 +15,26 @@
 </script>
 
 <template>
-    <div class="slide">
-        <div class="panel">
-            <div class="panelContainer__content">
-                <div class="panel__content">
-                    <div class="panel__text">
-                        <h2 class="panel__title">Elkyos</h2>
-                        <p class="panel__info">
-                            MMORPG médiéval fantastique, 2014
-                        </p>
-                    </div>
-                    <div class="panel__line"></div>
+    <div class="panel">
+        <div class="panelContainer__content">
+            <div class="panel__content">
+                <div class="panel__text">
+                    <h2 class="panel__title">Elkyos</h2>
+                    <p class="panel__info">
+                        MMORPG médiéval fantastique, 2014
+                    </p>
+                </div>
+                <div class="panel__line"></div>
+                <div class="panelContainer__link">
                     <router-link :to="{name: 'works'}" class="panel__link">En savoir plus</router-link>
                 </div>
             </div>
-            <div class="panelContainer__img">
-                <VImage class="panel__img" :src="ElkyosImage.src" :srcSet="ElkyosImage.srcSet"
-                        :styles="{backgroundImage: `url(${ElkyosImage.placeholder})`, width: '100%', height: '100%'}"
-                        alt="Description"></VImage>
-            </div>
+        </div>
+        <div class="panelContainer__img">
+            <VImage class="panel__img" :src="ElkyosImage.src" :srcSet="ElkyosImage.srcSet"
+                    sizes="500px"
+                    :styles="{backgroundImage: `url(${ElkyosImage.placeholder})`, minWidth: '100%', minHeight: '100%'}"
+                    alt="Description"></VImage>
         </div>
     </div>
 </template>
@@ -73,107 +43,126 @@
     @import "./../../sass/variables";
     @import "./../../sass/mixins/mixins";
 
-    .slide {
-        position: absolute;
-        width: 100%;
-        height: 100%;
-        @include no-user-select();
+    .panel {
+        display: block;
+        position: relative;
+        top: 50%;
+        left: 50%;
+        width: 50%;
+        max-width: 800px;
+        height: 466px;
+        transform: translate3d(-50%, -50%, 0);
 
-        .panel {
-            display: block;
-            position: relative;
-            top: 50%;
-            left: 50%;
-            width: 50%;
-            max-width: 800px;
-            height: 466px;
-            transform: translate3d(-50%, -50%, 0);
-
-            &:hover {
-                .panelContainer__content {
-                    .panel__content {
-                        .panel__link {
-                            opacity: 1;
-                            transform: translate3d(0, 0, 0);
-                            transition: transform .4s $easeOutCubic, opacity .4s $easeOutCubic;
-                        }
-                    }
-                }
-            }
-
+        &:hover {
             .panelContainer__content {
                 .panel__content {
-                    z-index: z(slidePanelContent);
-                    position: absolute;
-                    top: 26%;
-                    left: -8%;
-                    width: 115%;
-
                     .panel__text {
-                        display: inline-block;
-                        text-align: right;
-                        color: white;
+                        .panel__title, .panel__info {
+                            transform: translate3d(0, 100px, 0);
+                        }
 
                         .panel__title {
-                            font-size: 3em;
-                            font-family: font-family(Aventura);
-                            text-transform: uppercase;
-                            text-shadow: 0 2px 8px rgba(0, 0, 0, .4);
+                            transition: transform .4s $easeOutCubic;
                         }
 
                         .panel__info {
-                            position: relative;
-                            margin: 16px 0 0;
-                            text-shadow: 0 2px 8px rgba(0, 0, 0, .4);
+                            transition: transform .4s $easeOutCubic .03s;
 
                             &::before {
-                                content: '';
-                                display: inline-block;
-                                margin: 9px 14px 0 0;
-                                height: 1px;
-                                width: 30px;
-                                vertical-align: top;
-                                background-color: white;
+                                opacity: 0;
+                                transform: translate3d(150px, 0, 0);
+                                transition: transform .2s $easeOutCubic, opacity .15s $easeOutCubic;
                             }
                         }
                     }
 
-                    .panel__line {
-                        margin: 24px 0 0 25%;
-                        width: 75%;
-                        height: 3px;
-                        background-color: white;
-                        box-shadow: 0 2px 8px rgba(0, 0, 0, .4);
-                    }
-
-                    .panel__link {
-                        position: absolute;
-                        margin-top: 10px;
-                        right: 10%;
-                        color: white;
-                        opacity: 0;
-                        font-size: 1.2em;
-                        text-decoration: none;
-                        text-shadow: 0 2px 8px rgba(0, 0, 0, .4);
-                        transform: translate3d(0, -25px, 0);
-                        transition: transform .4s $easeOutCubic, opacity .2s $easeOutCubic;
+                    .panelContainer__link {
+                        .panel__link {
+                            transform: translate3d(0, -50px, 0);
+                            transition: transform .4s $easeOutCubic;
+                        }
                     }
                 }
             }
+        }
 
-            .panelContainer__img {
-                width: 100%;
-                height: 100%;
-                overflow: hidden;
-                box-shadow: 0px 20px 100.28px 8.72px rgba(0, 0, 0, .35);
+        .panelContainer__content {
+            .panel__content {
+                z-index: z(slidePanelContent);
+                position: absolute;
+                top: 25%;
+                left: -8%;
+                width: 115%;
 
-                .panel__img {
-                    display: block;
-                    width: 100%;
-                    min-height: 100%;
-                    transform: translate3d(0, -25%, 0);
-                    @include no-pointer-events();
+                .panel__text {
+                    display: inline-block;
+                    text-align: right;
+                    color: white;
+                    overflow: hidden;
+                    padding-bottom: 24px;
+
+                    .panel__title {
+                        font-size: 3em;
+                        font-family: font-family(Aventura);
+                        text-transform: uppercase;
+                        text-shadow: 0 2px 8px rgba(0, 0, 0, .4);
+                        transition: transform .4s $easeOutCubic;
+                    }
+
+                    .panel__info {
+                        position: relative;
+                        margin: 16px 0 0;
+                        text-shadow: 0 2px 8px rgba(0, 0, 0, .4);
+                        transition: transform .4s $easeOutCubic .2s;
+
+                        &::before {
+                            content: '';
+                            display: inline-block;
+                            margin: 9px 14px 0 0;
+                            height: 1px;
+                            width: 30px;
+                            opacity: 1;
+                            vertical-align: top;
+                            background-color: white;
+                            transition: transform .4s $easeOutCubic .3s, opacity .15s $easeOutCubic .4s;
+                        }
+                    }
                 }
+
+                .panel__line {
+                    margin-left: 25%;
+                    width: 75%;
+                    height: 3px;
+                    background-color: white;
+                    box-shadow: 0 2px 8px rgba(0, 0, 0, .4);
+                }
+
+                .panelContainer__link {
+                    position: absolute;
+                    padding-top: 10px;
+                    right: 10%;
+                    overflow: hidden;
+
+                    .panel__link {
+                        display: inline-block;
+                        color: white;
+                        font-size: 1.2em;
+                        text-decoration: none;
+                        text-shadow: 0 2px 8px rgba(0, 0, 0, .4);
+                        transition: transform .4s $easeOutCubic .4s;
+                    }
+                }
+            }
+        }
+
+        .panelContainer__img {
+            width: 100%;
+            height: 100%;
+            overflow: hidden;
+            box-shadow: 0px 20px 100.28px 8.72px rgba(0, 0, 0, .35);
+
+            .panel__img {
+                @include no-pointer-events();
             }
         }
     }
--- resources/assets/js/components/VWorks.vue
+++ resources/assets/js/components/VWorks.vue
@@ -1,18 +1,133 @@
 <script>
     import VSlide from '@components/VSlide';
+    import TiltFx from '@components/TiltFx';
+    import VClose from '@components/VClose';
 
     export default {
+        mounted() {
+            this.slideMouseContainer = document.getElementById('slideControlContainer__mouse');
+            this.slideMouse = this.slideMouseContainer.querySelector('.slideControl__mouse');
+            console.log(this.slideMouse)
+
+            const TiltFxInstance = new TiltFx(document.getElementById('slide__container'), {
+                mouseLeave: false
+            });
+
+            TiltFxInstance
+                .addElement('.panel__content', {
+                    translation: TiltFx.vector3(10, 10, 0),
+                    rotation: TiltFx.vector3(5, 5, 0),
+                    duration: 800,
+                    elasticity: 100,
+                    easing: 'easeOutCirc',
+                    reverseAnimation: {
+                        duration: 1200,
+                        easing: 'easeOutCirc',
+                        elasticity: 600
+                    }
+                })
+                .addElement('.panelContainer__img', {
+                    translation: TiltFx.vector3(5, 5, 0),
+                    rotation: TiltFx.vector3(5, 5, 0),
+                    duration: 800,
+                    elasticity: 400,
+                    easing: 'easeOutCirc',
+                    reverseAnimation: {
+                        duration: 1000,
+                        easing: 'easeOutCirc',
+                        elasticity: 800
+                    }
+                });
+        },
         components: {
-            VSlide
+            VSlide,
+            VClose
+        },
+        methods: {
+            slideTo(direction) {
+                console.log(arguments)
+            },
+            mousemove(ev) {
+                const x      = ev.clientX,
+                      y      = ev.clientY,
+                      rotate = ev.target.attributes['data-direction'].value === 'left' ? 0 : 180;
+
+                this.slideMouseContainer.style.setProperty('display', 'block');
+                this.slideMouseContainer.style.setProperty('transform', `translate3d(${x}px, ${y}px, 0)`);
+                this.slideMouse.style.setProperty('transform', `translate3d(-50%, -50%, 0) rotate(${rotate}deg)`);
+            },
+            mouseleave() {
+                this.slideMouseContainer.style.setProperty('display', 'none');
+            }
         }
     }
 </script>
 
 <template>
-    <div>
+    <div id="slide__container">
+        <div class="slide__control slide__control--left"
+             @click="slideTo('left')"
+             @mousemove="mousemove"
+             @mouseleave="mouseleave"
+             data-direction="left">
+        </div>
+        <div class="slide__control slide__control--right"
+             @click="slideTo('right')"
+             @mousemove="mousemove"
+             @mouseleave="mouseleave"
+             data-direction="right">
+        </div>
+
+        <VClose :to="{name: 'home'}"></VClose>
         <VSlide></VSlide>
+
+        <div id="slideControlContainer__mouse">
+            <span class="slideControl__mouse"><</span>
+        </div>
     </div>
 </template>
 
 <style lang="scss" scoped>
+    @import "./../../sass/mixins/mixins";
+
+    #slide__container {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        background-color: rgba(42, 42, 42, .2);
+        @include no-user-select();
+
+        #slideControlContainer__mouse {
+            display: none;
+            position: absolute;
+            top: 0;
+            left: 0;
+            cursor: none;
+            @include no-pointer-events();
+
+            .slideControl__mouse {
+                display: block;
+                color: #2a2a2a;
+                font-size: 5em;
+                transition: transform .4s $easeInOutQuint;
+                @include no-pointer-events();
+            }
+        }
+
+        .slide__control {
+            position: absolute;
+            top: 0;
+            height: 100%;
+            width: 50%;
+            cursor: none;
+
+            &--left {
+                left: 0;
+            }
+
+            &--right {
+                left: 50%;
+            }
+        }
+    }
 </style>
